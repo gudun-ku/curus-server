6215847f98ffe564b921e7281e7b62a4bdf99d18
Aleksandr Beloushkin <gudun.ku@gmail.com>
2019-09-28 19:11:51 +0300

    Removes not needed code

--- app.py
+++ app.py
@@ -45,17 +45,6 @@ def get_prediction(image_bytes):
     # return imagenet_class_index[predicted_idx]
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    if request.method == "POST":
-        file = request.files["file"]
-        img_bytes = file.read()
-    # class_id, class_name = get_prediction(image_bytes=img_bytes)
-    class_name = get_prediction(image_bytes=img_bytes)
-    return jsonify({"class_name": class_name})
-    #    return jsonify({"class_id": class_id, "class_name": class_name})
-
-
 @app.route("/predimg", methods=["POST"])
 def predimg():
     if request.method == "POST":
